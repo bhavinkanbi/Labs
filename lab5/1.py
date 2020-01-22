31e603e4a9d1956e5818b75d46fe21da8f8586ea
bunick <59815212+bunick@users.noreply.github.com>
2020-01-22 20:31:20 +0530

    Add files via upload

---
+++ lab5/1.py
@@ -0,0 +1,20 @@
+import math as m
+print(m.ceil(12.33))
+print(m.exp(20))
+print(m.fabs(-3))
+print(m.fabs(3))
+print(m.floor(12.33))
+print(m.log(10))
+print(m.modf(12.4))
+print(m.pow(2,10))
+print(m.sqrt(25))
+print(m.log10(10))
+x=10
+print(abs(x))
+print(max(1,2,3,4))
+print(min(1,2,3,4))
+print(round(0.75,1))
+print(round(-0.786,2))
+''' as the math fuction cmp will not display its result because in python 3
+ it has removed this function and hence we can't run it nor we can write it 
+ in the math function'''
